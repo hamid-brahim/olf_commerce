e87c91d885507747dbfc41162e4e4e7a5d4c7ab6
Hamid Ait Brahim <hamid.ait.brahim@thevaluechain.eu>
2022-11-07 13:50:23 +0100

    add resources for olf

---
+++ js-storefront/olf/node_modules/@spartacus/organization/administration/core/store/effects/permission.effect.d.ts
@@ -0,0 +1,17 @@
+import { Actions } from '@ngrx/effects';
+import { Observable } from 'rxjs';
+import { PermissionConnector } from '../../connectors/permission/permission.connector';
+import { OrganizationActions, PermissionActions } from '../actions';
+import * as i0 from "@angular/core";
+export declare class PermissionEffects {
+    private actions$;
+    private permissionConnector;
+    loadPermission$: Observable<PermissionActions.LoadPermissionSuccess | PermissionActions.LoadPermissionFail>;
+    loadPermissions$: Observable<PermissionActions.LoadPermissionsSuccess | PermissionActions.LoadPermissionSuccess | PermissionActions.LoadPermissionsFail>;
+    createPermission$: Observable<PermissionActions.CreatePermissionSuccess | PermissionActions.CreatePermissionFail | OrganizationActions.OrganizationClearData>;
+    updatePermission$: Observable<PermissionActions.UpdatePermissionSuccess | PermissionActions.UpdatePermissionFail | OrganizationActions.OrganizationClearData>;
+    loadPermissionTypes$: Observable<PermissionActions.LoadPermissionTypesSuccess | PermissionActions.LoadPermissionTypesFail>;
+    constructor(actions$: Actions, permissionConnector: PermissionConnector);
+    static ɵfac: i0.ɵɵFactoryDeclaration<PermissionEffects, never>;
+    static ɵprov: i0.ɵɵInjectableDeclaration<PermissionEffects>;
+}
